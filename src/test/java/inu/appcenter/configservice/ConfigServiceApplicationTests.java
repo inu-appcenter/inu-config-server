0196cc8473567aedfdba83e6e2b74b8d5f5f8f30
juhwan0815 <juwom0831@naver.com>
2022-01-13 18:17:59 +0900

    init

---
+++ src/test/java/inu/appcenter/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inu.appcenter.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
